06e3560d8bb56b5dcc24b8c6695539210ca6b50c
LG <lkshyagupta.002@gmail.com>
2025-07-29 00:50:47 +0530

    fix: minor bug

--- routers/scheduler.py
+++ routers/scheduler.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, BackgroundTasks, HTTPException, status
+from fastapi import APIRouter, BackgroundTasks
 import logging
 
 from managers.batch_processor import BatchProcessor
@@ -6,10 +6,9 @@ from managers.batch_processor import BatchProcessor
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
-batch_processor = BatchProcessor()
 
 @router.post("/schedule/process-notion-journals")
 async def schedule_notion_journal_processing(background_tasks: BackgroundTasks):
     logger.info("Received request to schedule Notion journal processing.")
-    background_tasks.add_task(batch_processor.process_notion_users_in_batches)
+    background_tasks.add_task(BatchProcessor().process_notion_users_in_batches)
     return {"message": "Notion journal processing scheduled in background."}
